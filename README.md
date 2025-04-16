5525262e654e1c9729a2d2e1ab694d7731719fb5
郭家栋 <1136108997@qq.com>
2025-04-16 17:45:25 +0800

    搭建基本框架

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# HPAMSD
-High-precision acoustic modeling with second-order staggered difference
+# High-precision acoustic modeling with second-order staggered difference
+
+本程序实现二阶交错差分声波模拟，使用PML边界条件，对比规则网格和交错网格的作用
---
+++ coeff.c
@@ -0,0 +1,15 @@
+/*
+ */
+#include <math.h>
+void get_sg_coeff(int n, float *a)
+{
+    float tmp1, tmp2;
+    float wgt;
+    int i, j;
+    for (int m = 0; m < n; m++)
+    {
+        j = m + 1;
+        tmp1 = 0;
+        tmp2 = 0;
+    }
+}
